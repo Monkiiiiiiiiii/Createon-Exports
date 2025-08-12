cdbadd3c69799e55393c130f7ebca9c4af362561
Monkiiiiiiiiii <150068187+Monkiiiiiiiiii@users.noreply.github.com>
2025-08-12 18:21:43 +0300

    Update index.service.worker.js

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1755011763|1091497505';
+const CACHE_VERSION = '1755012100|1428746506';
 /** @type {string} */
 const CACHE_PREFIX = 'Createon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
